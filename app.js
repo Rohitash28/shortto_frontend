210e790efc02b79e7f04c6ee6c46baa8f64cc4e6
Soumyajit Dutta <sanudatta11@gmail.com>
2017-10-28 17:04:01 +0530

    removed bug

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-var global_gradient  = null;
-var arr = ['red', 'meridian', 'sunset', 'timbr','pre-dawn','man-of-steel','instagram','sage','forest','deep-space','red-ocean','disco'];
+var global_gradient = null;
+var arr = ['red', 'meridian', 'sunset', 'timbr', 'pre-dawn', 'man-of-steel', 'instagram', 'sage', 'forest', 'deep-space', 'red-ocean', 'disco'];
 changeColor('random');
 
 function changeColor(color) {
 
     switch (color) {
         case 'red':
-               if(global_gradient != null)
-               {
-                    document.body.classList.remove(global_gradient);
-               }
-               document.body.classList.add('red');
-               global_gradient = 'red';
-               break;
-        case 'meridian':
-                if (global_gradient != null) {
-                    document.body.classList.remove(global_gradient);
-                }
-                document.body.classList.add('meridian');
-                global_gradient = 'meridian';
-                break;
-        case 'sunset':
-                if (global_gradient != null) {
-                    document.body.classList.remove(global_gradient);
-                }
-                document.body.classList.add('sunset');
-                global_gradient = 'sunset';
+            if (global_gradient != null) {
+                document.body.classList.remove(global_gradient);
+            }
+            document.body.classList.add('red');
+            global_gradient = 'red';
             break;
-            
+        case 'meridian':
+            if (global_gradient != null) {
+                document.body.classList.remove(global_gradient);
+            }
+            document.body.classList.add('meridian');
+            global_gradient = 'meridian';
+            break;
+        case 'sunset':
+            if (global_gradient != null) {
+                document.body.classList.remove(global_gradient);
+            }
+            document.body.classList.add('sunset');
+            global_gradient = 'sunset';
+            break;
+
         case 'timbr':
             if (global_gradient != null) {
                 document.body.classList.remove(global_gradient);
@@ -37,16 +36,18 @@ function changeColor(color) {
             break;
 
         case 'random':
-            do
-            {
-                var idx = Math.floor(Math.random() * arr.length);
-            }while(global_gradient == arr[idx]);
+            var idx = Math.floor(Math.random() * arr.length);
+
+            if (global_gradient == arr[idx]) {
+                idx = (idx + 1) % 11;
+            }
+
 
             if (global_gradient != null) {
                 document.body.classList.remove(global_gradient);
             }
-            
-            
+
+
             console.log(idx);
             document.body.classList.add(arr[idx]);
             global_gradient = arr[idx];
@@ -56,14 +57,12 @@ function changeColor(color) {
     }
 }
 
-function copy()
-{
+function copy() {
     var txt = document.getElementById('short_url');
     txt.select();
     document.execCommand("Copy");
     document.getElementById('label_done').innerHTML = 'Your Short URL is made.(Copied)';
-    setTimeout(function(){
+    setTimeout(function () {
         document.getElementById('label_done').innerHTML = 'Your Short URL is made.';
-    },2000)
+    }, 2000)
 }
-
